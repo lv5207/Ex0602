4e82e701582e3d8668f3b562e873ec000b205f3b
lv5207 <lv5207@bs.amalnet.k12.il>
2025-12-15 10:14:02 +0200

    Part 4

--- app/src/main/java/com/example/ex0602/MainActivity.java
+++ app/src/main/java/com/example/ex0602/MainActivity.java
@@ -27,10 +27,9 @@ public class MainActivity extends AppCompatActivity {
         count++;
         btn.setText("This is a click number: "+count);
 
-        if (count == 7)
+        if (count % 7 == 0)
         {
-            btn.setText("Enough to click. Go to new start!");
-            count = 0;
+            btn.setText("BOOM !");
         }
     }
 }
