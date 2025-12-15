f1d8789793af306cb291502fef6a8522fdaa5162
lv5207 <lv5207@bs.amalnet.k12.il>
2025-12-15 10:05:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex0602/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ex0602;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+    }
+
+    public void clicked(View view) {
+        btn.setText("Oh, yea, I've been clicked!");
+    }
+}
